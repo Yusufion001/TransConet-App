9345520a4809601b000c95395dedd1d721f7c987
Yusuf <128279675+Yusufion001@users.noreply.github.com>
2026-08-07 09:03:03 +0100

    Create userSimulator.ts

---
+++ frontend/src/hooks/userSimulator.ts
@@ -0,0 +1,14 @@
+const [statusBarTime, setStatusBarTime] = useState('09:41');
+
+const [simulatedCarrier] = useState('MTN NG 5G');
+
+const [isMobilePortalOpen, setIsMobilePortalOpen] = useState(false);
+
+const [expoTunnelUrl, setExpoTunnelUrl] = useState(window.location.href);
+
+const [copied, setCopied] = useState(false);
+handleCopyLink()
+
+useEffect(() => {
+    const updateTime...
+})
