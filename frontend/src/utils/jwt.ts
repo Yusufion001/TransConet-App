503e67da86b84197f60373041a11c3a8f935edf8
Yusuf <128279675+Yusufion001@users.noreply.github.com>
2026-08-07 08:48:58 +0100

    Update jwt.ts

--- frontend/src/utils/jwt.ts
+++ frontend/src/utils/jwt.ts
@@ -1,1 +1,26 @@
-parseJwt()
+function parseJwt(token: string) {
+  try {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+
+    const jsonPayload = decodeURIComponent(
+      window.atob(base64)
+        .split('')
+        .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+        .join('')
+    );
+
+    const payload = JSON.parse(jsonPayload);
+
+    if (payload.exp && payload.exp * 1000 < Date.now()) {
+      return null;
+    }
+
+    return payload;
+
+  } catch {
+    return null;
+  }
+}
+
+export default parseJwt;
