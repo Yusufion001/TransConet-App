824c4953c78dc55873e55f001003e2b01b70c9db
Yusuf <128279675+Yusufion001@users.noreply.github.com>
2026-08-09 17:49:54 +0100

    Apply shipper mobile UI styling layer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,6 +8,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './royal-blue-theme.css';
+import './styles/shipper-mobile.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ErrorBoundary } from './components/ErrorBoundary';
 
