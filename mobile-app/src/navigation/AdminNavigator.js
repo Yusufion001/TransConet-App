052060532b46dbe697aafc6cb10490f80c2c3c08
Yusuf <128279675+Yusufion001@users.noreply.github.com>
2026-08-12 09:46:41 +0100

    feat: add native admin account route

--- mobile-app/src/navigation/AdminNavigator.js
+++ mobile-app/src/navigation/AdminNavigator.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AdminDashboardScreen from '../screens/admin/AdminDashboardScreen';
 import AdminModuleScreen from '../screens/admin/AdminModuleScreen';
+import AdminAccountsScreen from '../screens/admin/AdminAccountsScreen';
 
 const Stack = createNativeStackNavigator();
-
-export default function AdminNavigator() {
-  return <Stack.Navigator screenOptions={{ headerShown: false }}>
-    <Stack.Screen name="AdminDashboard" component={AdminDashboardScreen} />
-    <Stack.Screen name="AdminModule" component={AdminModuleScreen} />
+export default function AdminNavigator(){
+  return <Stack.Navigator screenOptions={{headerShown:false}}>
+    <Stack.Screen name="AdminDashboard" component={AdminDashboardScreen}/>
+    <Stack.Screen name="AdminModule" component={AdminModuleScreen}/>
+    <Stack.Screen name="AdminAccounts" component={AdminAccountsScreen}/>
   </Stack.Navigator>;
 }
