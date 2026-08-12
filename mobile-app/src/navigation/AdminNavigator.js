0922b1b890e539dc00da1c01e488ec6fe5502107
Yusuf <128279675+Yusufion001@users.noreply.github.com>
2026-08-12 09:44:47 +0100

    feat: add native administration navigator

---
+++ mobile-app/src/navigation/AdminNavigator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AdminDashboardScreen from '../screens/admin/AdminDashboardScreen';
+import AdminModuleScreen from '../screens/admin/AdminModuleScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function AdminNavigator() {
+  return <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="AdminDashboard" component={AdminDashboardScreen} />
+    <Stack.Screen name="AdminModule" component={AdminModuleScreen} />
+  </Stack.Navigator>;
+}
